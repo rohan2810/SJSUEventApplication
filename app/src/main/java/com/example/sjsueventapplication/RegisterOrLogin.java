6434087e0eeacdf399c0dfd8ca1a18f349e18a24
rohan <rohansurana2810@gmail.com>
2020-04-07 22:26:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sjsueventapplication/RegisterOrLogin.java
@@ -0,0 +1,14 @@
+package com.example.sjsueventapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterOrLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
